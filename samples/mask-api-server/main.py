871f1559a60ecde1ef6ed2138f967cd2c1bfa4cd
Ishraq Hasan <ishraq10199@gmail.com>
2022-07-31 07:45:48 +0600

    Second Commit

---
+++ samples/mask-api-server/main.py
@@ -0,0 +1,197 @@
+from typing import Union
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+import uuid
+import cv2
+import matplotlib.pyplot as plt
+import io
+import os
+import sys
+import base64
+import cv2
+import numpy as np
+from PIL import Image
+import time
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+import skimage
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+
+ROOT_DIR = '../../Mask_RCNN'
+assert os.path.exists(
+    ROOT_DIR), 'ROOT_DIR does not exist. Did you forget to read the instructions above? ;)'
+
+# Import mrcnn libraries
+sys.path.append(ROOT_DIR)
+import mrcnn.model as modellib
+from mrcnn import visualize
+import mrcnn.utils as utils
+from mrcnn.config import Config
+
+
+class TargetConfig(Config):
+    """Configuration for training on the cigarette butts dataset.
+    Derives from the base Config class and overrides values specific
+    to the cigarette butts dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "Target"
+
+    # Train on 1 GPU and 1 image per GPU. Batch size is 1 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # background + 1 (Target)
+
+    # All of our training images are 512x512
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+
+    # You can experiment with this number to see if it improves training
+    STEPS_PER_EPOCH = 500
+
+    # This is how often validation is run. If you are using too much hard drive space
+    # on saved models (in the MODEL_DIR), try making this value larger.
+    VALIDATION_STEPS = 50
+
+    # Matterport originally used resnet101, but I downsized to fit it on my graphics card
+    BACKBONE = 'resnet101'
+
+    # To be honest, I haven't taken the time to figure out what these do
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+    TRAIN_ROIS_PER_IMAGE = 32
+    MAX_GT_INSTANCES = 50
+    POST_NMS_ROIS_INFERENCE = 500
+    POST_NMS_ROIS_TRAINING = 1000
+
+
+class InferenceConfig(TargetConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    DETECTION_MIN_CONFIDENCE = 0.89
+
+
+inference_config = InferenceConfig()
+MODEL_DIR = os.path.join(os.path.abspath(".."), "samples", "logs")
+# Recreate the model in inference mode
+model = modellib.MaskRCNN(mode="inference",
+                          config=inference_config,
+                          model_dir=MODEL_DIR)
+model_path = os.path.join(
+    MODEL_DIR, "target20220718T1633", "mask_rcnn_target_0200.h5")
+# Load trained weights (fill in path to trained weights here)
+assert model_path != "", "Provide path to trained weights"
+print("Loading weights from ", model_path)
+model.load_weights(model_path, by_name=True)
+
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
+
+app = FastAPI()
+
+@app.post("/files/")
+async def create_file(file: Union[bytes, None] = File(default=None)):
+    if not file:
+        return {"message": "No file sent"}
+    else:
+        return {"file_size": len(file)}
+
+@app.post("/crop_target/")
+async def crop_target(file: Union[UploadFile, None] = None):
+    if not file:
+        return {"message": "No upload file sent"}
+    else:
+        # READ IMAGE
+        img = skimage.io.imread(await file.read(), plugin='imageio')
+        
+        # PROCESS IMAGE
+        img_arr = np.array(img)
+        results = model.detect([img_arr], verbose=0)
+        r = results[0]
+        [[ymin, xmin, ymax, xmax]] = r['rois']
+        d_img = img.copy()
+        img = d_img[:, :, ::-1]
+        pts = np.array([(xmin, ymin), (xmax, ymin),
+                       (xmin, ymax), (xmax, ymax)], dtype="float32")
+        warped = four_point_transform(img, pts)
+        warped = cv2.resize(warped, (max(warped.shape), max(
+            warped.shape)), interpolation=cv2.INTER_AREA)
+
+        warped = cv2.cvtColor(warped, cv2.COLOR_BGR2RGB)
+
+        # SEND RESPONSE AS ANOTHER IMAGE
+        img_pil = Image.fromarray(warped)
+        imgio_shrunken = io.BytesIO()
+        img_pil.save(imgio_shrunken, 'JPEG')
+        imgio_shrunken.seek(0)
+
+        return StreamingResponse(content=imgio_shrunken, media_type="image/jpeg")
+
